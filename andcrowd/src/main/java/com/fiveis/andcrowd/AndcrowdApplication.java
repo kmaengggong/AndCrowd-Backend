f417f9a60f64731ea314cf7b306af99c9ef57e88
kmaengggong <kmaengggong@gmail.com>
2023-08-01 10:49:41 +0900

    김명호: 프로젝트 초기화 다시

---
+++ andcrowd/src/main/java/com/fiveis/andcrowd/AndcrowdApplication.java
@@ -0,0 +1,13 @@
+package com.fiveis.andcrowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndcrowdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndcrowdApplication.class, args);
+	}
+
+}
---
+++ andcrowd/src/test/java/com/fiveis/andcrowd/AndcrowdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiveis.andcrowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndcrowdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
