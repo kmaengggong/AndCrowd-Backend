49ed8f5e854dc9564a0e062e829837f49515bf0c
양현성 <125037243+Void-OvO@users.noreply.github.com>
2023-08-02 23:23:47 +0900

    양현성 : AndRepository 생성

---
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/AndRepository.java
@@ -0,0 +1,20 @@
+package com.fiveis.andcrowd.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.fiveis.andcrowd.entity.And;
+
+import java.util.List;
+
+@Mapper
+public interface AndRepository {
+    List<And> findAll();
+
+    And findById (int andId);
+    void save(And and);
+
+    void deleteById(int andId);
+
+    void update(And and);
+
+
+}
