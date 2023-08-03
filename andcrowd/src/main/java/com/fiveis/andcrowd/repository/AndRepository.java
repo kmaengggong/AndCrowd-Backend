5f8ab3bc053fa1cf3d51f694e5ba53f040a21e59
양현성 <didgustjd31349@naver.com>
2023-08-03 11:53:49 +0900

    양현성:AndRepository JPA로 수정

--- andcrowd/src/main/java/com/fiveis/andcrowd/repository/AndRepository.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/AndRepository.java
@@ -1,19 +1,12 @@
 package com.fiveis.andcrowd.repository;
 
-import org.apache.ibatis.annotations.Mapper;
 import com.fiveis.andcrowd.entity.And;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
-public interface AndRepository {
-    List<And> findAllAnd();
-
-    And findById(int andId);
-
-    void save(And and);
-
-    void deleteById(int andId);
-
-    void update(And and);
+@Repository
+public interface AndRepository extends JpaRepository<And, Integer> {
+    List<And> findAllByUserId(int userId);
 }
