e95b43a1978a492e3a06cba144699a06f43cdef0
임규빈 <gyublayla@gmail.com>
2023-08-03 17:40:37 +0900

    임규빈: AndCategory 어노테이션 추가

--- andcrowd/src/main/java/com/fiveis/andcrowd/entity/AndCategory.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/entity/AndCategory.java
@@ -1,18 +1,21 @@
 package com.fiveis.andcrowd.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
-@Builder
-@ToString
-@AllArgsConstructor
+@Getter @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder @Entity
 public class AndCategory {
 
+    @Id
+    @Column(nullable = false)
     private int andCategoryId;
 
+    @Column(nullable = false)
     private int andCategoryName;
 
+    @Column(nullable = false)
     private boolean isDeleted;
 }
