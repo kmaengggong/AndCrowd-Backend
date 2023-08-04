e987e40205d4d83d3e9b95f60a313c3e002b8e9d
kmaengggong <kmaengggong@gmail.com>
2023-08-04 15:59:09 +0900

    김명호: login, signup 기능 및 AccessToken 구현

--- andcrowd/src/main/java/com/fiveis/andcrowd/Service/UserService.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/Service/UserService.java
@@ -3,35 +3,44 @@ package com.fiveis.andcrowd.service;
 import com.fiveis.andcrowd.entity.User;
 import com.fiveis.andcrowd.repository.UserJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl {
+public class UserService {
     private final UserJPARepository userJPARepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserJPARepository userJPARepository){
+    public UserService(UserJPARepository userJPARepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder){
         this.userJPARepository = userJPARepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    Optional<User> findById(int userId){
+    public Optional<User> findById(int userId){
         return userJPARepository.findById(userId);
     }
 
-    void deleteById(int userId) {
+    public void deleteById(int userId) {
         userJPARepository.deleteById(userId);
     }
 
-    void save(User user) {
+    public void save(User user) {
+        user.updateUser(bCryptPasswordEncoder.encode(user.getUserPassword()), user.getUserPhone(), user.getUserProfileImg());
         userJPARepository.save(user);
     }
 
-    void update(User user){
+    public void update(User user){
         int userId = user.getUserId();
         User originUser = userJPARepository.findById(userId).get();
         originUser.updateUser(user.getUserPassword(), user.getUserPhone(), user.getUserProfileImg());
         userJPARepository.save(originUser);
+    }
+
+    public User getByCredentials(String userEmail){
+        return userJPARepository.findByUserEmail(userEmail);
     }
 }
--- andcrowd/src/main/java/com/fiveis/andcrowd/entity/User.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/entity/User.java
@@ -3,15 +3,23 @@ package com.fiveis.andcrowd.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @ToString
 @NoArgsConstructor
-public class User {
+@Table(name="users")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;  // 유저 ID
@@ -23,7 +31,7 @@ public class User {
     private String userPassword;
 
     @Column(nullable = false)
-    private String userName;  // 이름
+    private String userKorName;  // 이름
 
     @Column(nullable = false)
     private String userNickname;  // 닉네임
@@ -34,10 +42,11 @@ public class User {
     private String userProfileImg;  // 프로필 이미지 경로
 
     @Column(nullable = false)
-    private Date userBirth;  // 생년월일
+    private LocalDateTime userBirth;  // 생년월일
 
     @Column(nullable = false)
-    private Date userRegister;  // 가입일
+    @CreationTimestamp
+    private LocalDateTime userRegister;  // 가입일
 
     @Column(nullable = false)
     private int userTos;  // 서비스 이용약관 동의
@@ -52,32 +61,62 @@ public class User {
     private Authority authority;  // 관리자 권한 여부
 
     @Builder
-    public User(int userId, String userEmail, String userPassword, String userName, String userNickname,
-                String userPhone, String userProfileImg, Date userBirth, Date userRegister,
+    public User(int userId, String userEmail, String userPassword, String userKorName, String userNickname,
+                String userPhone, String userProfileImg, LocalDateTime userBirth, LocalDateTime userRegister,
                 int userTos, int userPrivacy, int userMarketing, Authority authority){
         this.userId = userId;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
-        this.userName = userName;
+        this.userKorName = userKorName;
         this.userNickname = userNickname;
         this.userPhone = userPhone;
         this.userProfileImg = userProfileImg;
         this.userBirth = userBirth;
-        this.userRegister = userRegister;
+        this.userRegister = userRegister == null ? LocalDateTime.now() : userRegister;
         this.userTos = userTos;
         this.userPrivacy = userPrivacy;
         this.userMarketing = userMarketing;
         this.authority = authority;
     }
 
-    @PrePersist
-    public void setDefaultValue(){
-        this.userRegister = new Date(new java.util.Date().getTime());
-    }
-
     public void updateUser(String userPassword, String userPhone, String userProfileImg){
         this.userPassword = userPassword;
         this.userPhone = userPhone;
         this.userProfileImg = userProfileImg;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(("user")));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
