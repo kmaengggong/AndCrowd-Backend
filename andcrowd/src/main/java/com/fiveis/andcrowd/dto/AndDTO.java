0d3a77d1e193b30d69ee0fab7529e186ecefde6a
양현성 <didgustjd31349@naver.com>
2023-08-07 10:12:53 +0900

    양현성: AndDTO 코드 수정

--- andcrowd/src/main/java/com/fiveis/andcrowd/dto/AndDTO.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/dto/AndDTO.java
@@ -85,6 +85,8 @@ public class AndDTO {
     }
     
     //모임 게시판
+    @Getter @Setter @Builder @ToString
+    @AllArgsConstructor @NoArgsConstructor
     public static class DynamicAndBoardDTO {
         private int andBoardId;
         private int andId;
@@ -98,50 +100,25 @@ public class AndDTO {
         private Integer andBoardViewCount;
         private boolean isDeleted;
 
-        public DynamicAndBoardDTO(int andBoardId, int andId, int userId, Integer andBoardTag,
-                                  String andBoardTitle, String andBoardContent, String andImg,
-                                  LocalDateTime publishedAt, LocalDateTime updatedAt,
-                                  Integer andBoardViewCount, boolean isDeleted) {
-            this.andBoardId = andBoardId;
-            this.andId = andId;
-            this.userId = userId;
-            this.andBoardTag = andBoardTag;
-            this.andBoardTitle = andBoardTitle;
-            this.andBoardContent = andBoardContent;
-            this.andImg = andImg;
-            this.publishedAt = publishedAt;
-            this.updatedAt = updatedAt;
-            this.andBoardViewCount = andBoardViewCount;
-            this.isDeleted = isDeleted;
-        }
     }
 
     //모임 분야
+    @Getter @Setter @Builder @ToString
+    @AllArgsConstructor @NoArgsConstructor
     public class AndCategoryDTO {
         private int andCategoryId;
         private String andCategoryName;
         private boolean isDeleted;
-
-        public AndCategoryDTO(int andCategoryId, String andCategoryName, boolean isDeleted) {
-            this.andCategoryId = andCategoryId;
-            this.andCategoryName = andCategoryName;
-            this.isDeleted = isDeleted;
-        }
     }
 
     //모임 참여자
+    @Getter @Setter @Builder @ToString
+    @AllArgsConstructor @NoArgsConstructor
     public class DynamicAndMemberDTO {
         private int memberId;
         private int andId;
         private int userId;
         private boolean isDeleted;
-
-        public DynamicAndMemberDTO(int memberId, int andId, int userId, boolean isDeleted) {
-            this.memberId = memberId;
-            this.andId = andId;
-            this.userId = userId;
-            this.isDeleted = isDeleted;
-        }
     }
 
 }
