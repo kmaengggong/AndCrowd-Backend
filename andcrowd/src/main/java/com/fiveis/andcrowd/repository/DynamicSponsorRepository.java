269bcd9c2d1e5852862b167465b0b661f012b430
songyoh <code6207@gmail.com>
2023-08-10 15:40:34 +0900

    오송이 : Dynamic(Sponsor)Repo, DTO 변수명 수정 및 오타 수정

---
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/DynamicSponsorRepository.java
@@ -0,0 +1,27 @@
+package com.fiveis.andcrowd.repository;
+
+import com.fiveis.andcrowd.dto.DynamicSponsorDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DynamicSponsorRepository {
+
+    List<DynamicSponsorDTO.FindById> findAll(int crowdId);
+
+    DynamicSponsorDTO.FindById findBySponsorId(@Param("crowd_id") int crowdId, @Param("sponsor_id") int sponsorId);
+
+    List<DynamicSponsorDTO.FindById> findByUserId(@Param("crowd_id") int crowdId, @Param("user_id") int userId);
+
+    void save(DynamicSponsorDTO.Update dynamicSponsorInsertDTO);
+
+    void update(DynamicSponsorDTO.Update dynamicSponsordUpdateDTO);
+
+    void deleteBySponsorId(@Param("crowd_id") int crowdId, @Param("sponsor_id") int sponsorId);
+
+    void createRewardTable();
+    void dropRewardTable();
+    void insertTestData();
+}
