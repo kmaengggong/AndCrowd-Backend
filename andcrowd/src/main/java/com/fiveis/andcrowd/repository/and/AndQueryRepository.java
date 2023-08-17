97e9e654724c5c2f6bde828c7fbc293d751a95c1
임규빈 <gyublayla@gmail.com>
2023-08-17 14:31:43 +0900

    임규빈: AndQueryRepository  구현 및 테스트(And 페이지 무한 스크롤)

---
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/and/AndQueryRepository.java
@@ -0,0 +1,13 @@
+package com.fiveis.andcrowd.repository.and;
+
+import com.fiveis.andcrowd.dto.and.AndDTO;
+import com.fiveis.andcrowd.entity.and.And;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AndQueryRepository {
+    Slice<AndDTO.Find> findAllByCategoryAndStatusAndSort(
+            Integer categoryId, Integer andStatus, String sortField, String sortOrder, Pageable pageable);
+}
---
+++ andcrowd/src/test/resources/dummy_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO and_table (and_id, user_id, and_category_id, crowd_id, and_title, and_header_img, and_content, and_end_date, need_num_mem, and_img1, and_img2, and_img3, and_img4, and_img5, published_at, updated_at, and_like_count, and_view_count, and_status, ad_id, is_deleted)
+VALUES
+    (NULL, 1, 1, 321, "andtitle1", "headerimgdir1", "andcontent1", '2023-08-16 09:45:00', 2, null, null, null, null, null, now(), now(), 10, 15, 0, 0, false),
+    (NULL, 10, 4, 323, "andtitle2", "headerimgdir2", "andcontent2", '2023-08-17 10:30:00', 3, null, null, null, null, null, now(), now(), 8, 20, 1, 0, false),
+    (NULL, 1, 1, 326, "andtitle3", "headerimgdir3", "andcontent3", '2023-08-18 11:15:00', 4, null, null, null, null, null, now(), now(), 15, 25, 0, 0, false),
+    (NULL, 4, 8, 329, "andtitle4", "headerimgdir4", "andcontent4", '2023-08-19 12:00:00', 5, null, null, null, null, null, now(), now(), 12, 18, 1, 0, false),
+    (NULL, 5, 3, 332, "andtitle5", "headerimgdir5", "andcontent5", '2023-08-20 13:30:00', 2, null, null, null, null, null, now(), now(), 20, 22, 0, 0, false),
+    (NULL, 1, 9, 335, "andtitle6", "headerimgdir6", "andcontent6", '2023-08-21 14:45:00', 3, null, null, null, null, null, now(), now(), 18, 30, 1, 0, false),
+    (NULL, 7, 1, 338, "andtitle7", "headerimgdir7", "andcontent7", '2023-08-22 15:20:00', 4, null, null, null, null, null, now(), now(), 25, 28, 0, 0, false),
+    (NULL, 8, 6, 341, "andtitle8", "headerimgdir8", "andcontent8", '2023-08-23 16:10:00', 5, null, null, null, null, null, now(), now(), 30, 32, 1, 0, false),
+    (NULL, 2, 7, 344, "andtitle9", "headerimgdir9", "andcontent9", '2023-08-24 17:00:00', 2, null, null, null, null, null, now(), now(), 22, 26, 0, 0, false),
+    (NULL, 3, 10, 347, "andtitle10", "headerimgdir10", "andcontent10", '2023-08-25 18:30:00', 3, null, null, null, null, null, now(), now(), 28, 35, 1, 0, false);
