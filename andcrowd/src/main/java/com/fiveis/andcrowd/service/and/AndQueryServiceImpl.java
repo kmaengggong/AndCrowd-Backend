2a6f14773382cf6582b8ccdd486c1d33003141ba
임규빈 <gyublayla@gmail.com>
2023-08-17 15:33:37 +0900

    임규빈: And 엔티티 기본값 수정

--- andcrowd/src/main/java/com/fiveis/andcrowd/service/and/AndQueryServiceImpl.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/service/and/AndQueryServiceImpl.java
@@ -2,6 +2,7 @@ package com.fiveis.andcrowd.service.and;
 
 import com.fiveis.andcrowd.dto.and.AndDTO;
 import com.fiveis.andcrowd.repository.and.AndQueryRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Service;
 public class AndQueryServiceImpl implements AndQueryService{
     private final AndQueryRepository andQueryRepository;
 
-    public AndQueryServiceImpl(AndQueryRepository andQueryRepository) {
+    public AndQueryServiceImpl(@Qualifier("andQueryRepositoryImpl") AndQueryRepository andQueryRepository) {
         this.andQueryRepository = andQueryRepository;
     }
-    
+
     @Override
     public Slice<AndDTO.Find> findAllByCategoryAndStatusAndSort(Integer categoryId, Integer andStatus, String sortField, String sortOrder, Pageable pageable) {
         return andQueryRepository.findAllByCategoryAndStatusAndSort(categoryId, andStatus, sortField, sortOrder, pageable);
