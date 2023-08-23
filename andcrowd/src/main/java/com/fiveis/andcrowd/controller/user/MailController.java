96c3b1d33a667b94c99871d186fcf1e62c8e29ee
kmaengggong <kmaengggong@gmail.com>
2023-08-23 17:22:13 +0900

    김명호: 메인 인증 Service, Controller 구현:

--- andcrowd/src/main/java/com/fiveis/andcrowd/controller/user/MailController.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/controller/user/MailController.java
@@ -1,16 +1,30 @@
 package com.fiveis.andcrowd.controller.user;
 
+import com.fiveis.andcrowd.service.user.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class MailController {
+    private final MailService mailService;
+
+    @Autowired
+    public MailController(MailService mailService){
+        this.mailService = mailService;
+    }
+
     @RequestMapping(value="/mailAuth", method=RequestMethod.POST)
-    public ResponseEntity<?> getEmailAuthNumber(@RequestBody String userEmail){
-        System.out.println(userEmail);
-        return null;
+    public ResponseEntity<?> getEmailAuthNumber(@RequestBody Map<String, String> userEmail){
+        System.out.println(userEmail.get("userEmail"));
+        String res = mailService.setEmail(userEmail.get("userEmail"));
+        System.out.println(res);
+        if(res == null) return ResponseEntity.badRequest().body("Sth wrong dude");
+        return ResponseEntity.ok(res);
     }
 }
