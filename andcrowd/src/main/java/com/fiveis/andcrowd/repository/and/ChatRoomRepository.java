8e8fadb1dbea1f7a524b9620d9af7eee745a0f71
kmaengggong <kmaengggong@gmail.com>
2023-09-15 17:13:00 +0900

    김명호: 브랜치 분리 전 커밋

--- andcrowd/src/main/java/com/fiveis/andcrowd/repository/and/ChatRoomRepository.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/and/ChatRoomRepository.java
@@ -1,7 +1,7 @@
 package com.fiveis.andcrowd.repository.and;
 
 import com.fiveis.andcrowd.entity.and.ChatRoom;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
