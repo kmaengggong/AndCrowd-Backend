4bdd9cb8373b5f17d8bd11d9cb1874039fcffd7b
songyoh <code6207@gmail.com>
2023-09-18 15:58:51 +0900

    오송이 : crowd board, qna, qnaReply 수정

--- andcrowd/src/main/java/com/fiveis/andcrowd/repository/crowd/DynamicCrowdQnaRepository.java
+++ andcrowd/src/main/java/com/fiveis/andcrowd/repository/crowd/DynamicCrowdQnaRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface DynamicCrowdQnaRepository {
-    void createDynamicCrowdQnaTable(@Param("crowdId") int crowdId);
+    void createDynamicCrowdQnaTable(int crowdId);
 
     List<DynamicCrowdQnaDTO.Find> findAll(int crowdId);
 
